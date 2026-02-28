465fe1cb573ee642e710e2b3699e38f0e1c48d08
MythicalRev <landonsciara@icloud.com>
2026-02-27 21:42:45 -0500

    Fixed Buttons Visible During Playtest

--- src/includes/modifies/EditorUIModify.cpp
+++ src/includes/modifies/EditorUIModify.cpp
@@ -8,27 +8,47 @@
 using namespace geode::prelude;
 
 class $modify(BEPEditorUI, EditorUI) {
+    struct Fields {
+        CCMenuItemSpriteExtra* m_addPosBtn;
+        CCMenuItemSpriteExtra* m_posMenuBtn;
+    };
+
     static void onModify(auto& self) {
         (void) self.setHookPriority("EditorUI::init", Priority::VeryLatePost);
     }
+
     bool init(LevelEditorLayer* editorLayer) {
         if (!EditorUI::init(editorLayer)) return false;
         
         auto addEditorPosSpr = CCSprite::createWithSpriteFrameName("GJ_plusBtn_001.png");
         addEditorPosSpr->setScale(.8f);
-        auto addEditorPosBtn = CCMenuItemSpriteExtra::create(addEditorPosSpr, this, menu_selector(BEPEditorUI::onPosAdded));
-        addEditorPosBtn->setID("addpos-btn"_spr);
+        m_fields->m_addPosBtn = CCMenuItemSpriteExtra::create(addEditorPosSpr, this, menu_selector(BEPEditorUI::onPosAdded));
+        m_fields->m_addPosBtn->setID("addpos-btn"_spr);
 
         auto posMenuSpr = CCSprite::createWithSpriteFrameName("GJ_practiceBtn_001.png");
         posMenuSpr->setScale(.6f);
-        auto posMenuBtn = CCMenuItemSpriteExtra::create(posMenuSpr, this, menu_selector(BEPEditorUI::onMenuOpened));
-        posMenuBtn->setID("posmenu-btn"_spr);
+        m_fields->m_posMenuBtn = CCMenuItemSpriteExtra::create(posMenuSpr, this, menu_selector(BEPEditorUI::onMenuOpened));
+        m_fields->m_posMenuBtn->setID("posmenu-btn"_spr);
 
-        this->getChildByID("settings-menu")->addChild(addEditorPosBtn);
-        this->getChildByID("settings-menu")->addChild(posMenuBtn);
+        this->getChildByID("settings-menu")->addChild(m_fields->m_addPosBtn);
+        this->getChildByID("settings-menu")->addChild(m_fields->m_posMenuBtn);
         this->getChildByID("settings-menu")->updateLayout();
 
         return true;
+    }
+
+    void onPlaytest(CCObject* sender) {
+        EditorUI::onPlaytest(sender);
+
+        m_fields->m_addPosBtn->setVisible(false);
+        m_fields->m_posMenuBtn->setVisible(false);
+    }
+
+    void playtestStopped() {
+        EditorUI::playtestStopped();
+
+        m_fields->m_addPosBtn->setVisible(true);
+        m_fields->m_posMenuBtn->setVisible(true);
     }
 
     void onPosAdded(CCObject* sender) {
